00139ac50a94c75f7d39f2a2a3f10a5a3132d240
h1994st <h1994st@gmail.com>
2014-03-27 12:59:59 +0800

    weibo login

--- Co_Trip/settings.py
+++ Co_Trip/settings.py
@@ -83,6 +83,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.middleware.locale.LocaleMiddleware',
+    'social.apps.django_app.middleware.SocialAuthExceptionMiddleware',
 )
 
 
